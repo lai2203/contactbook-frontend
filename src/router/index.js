c10a5eaf2c9a55825328b7639dd61d0fae901b17
vanlai <nvlai@example.com>
2022-04-02 20:24:38 +0700

    khoi tao du an

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createWebHistory, createRouter } from "vue-router";
+import ContactBook from "../views/ContactBook.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "ContactBook",
+        component: ContactBook,
+    },
+    {
+        path: "/contacts/:id",
+        name: "EditContact",
+        component: () => import("../views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+});
+
+export default router;
